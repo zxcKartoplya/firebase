b0c738d31e7d53920d4105c90b4b75f0533f1d7c
Kirill <bel-kir05@mail.ru>
2025-02-27 12:39:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>  
+  <div>  
+    <h1>Данные из Firestore</h1>  
+    {{ items }}
+  </div>  
+</template>  
+
+<script setup>  
+import { collection, getDocs } from "firebase/firestore"
+import { onMounted, ref } from 'vue'
+import { db } from "./firebase"
+
+const items = ref([]);   
+
+onMounted(async () => {  
+  const querySnapshot = await getDocs(collection(db, "test"));  
+  items.value = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));   
+});  
+</script>  
+
+<style>  
+/* Ваши стили */  
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import { initializeApp } from "firebase/app";
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
